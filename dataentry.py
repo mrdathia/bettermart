51858ebaaa509b5edac4099795728ac62fbc26c1
Shubham Kishor Dathia <54986627+mrdathia@users.noreply.github.com>
2023-03-25 14:50:56 +0530

    Update dataentry.py

--- dataentry.py
+++ dataentry.py
@@ -1,4 +1,28 @@
 import pandas as pd
+from geopy.geocoders import Nominatim
 
-data = pd.read_xlsx('filteredData.xlsx')
-data.head()
+import http
+
+geolocator = Nominatim(user_agent="bettermart")
+
+data = pd.read_excel('filteredData1.xlsx')
+#
+latitude = list(data["latitude"])
+date = list(data["date"])
+longitude = list(data["longitude"])
+#
+# res = {i: {"date": date[i], "latitude": latitude[i], "longitude": longitude[i]} for i in range(len(latitude))}
+#
+# print(res)
+
+"A9SCBHEF4SHU75SWVQJDV8RST&contentType=json"
+print("started")
+pincodes=list(range(len(latitude)))
+for i in range(len(latitude)):
+    pincodes[i] = (geolocator.reverse(f"{latitude[i]},{longitude[i]}")).raw["address"]["postcode"]
+    print(f"{i}:  {latitude[i]},{longitude[i]}: {pincodes[i]}")
+# pincodes = [(geolocator.reverse(f"{latitude[i]},{longitude[i]}")).raw["address"]["postcode"] for i in
+#             range(len(latitude))]
+print(pincodes)
+print("ended")
+data["pincode"] = pincodes
